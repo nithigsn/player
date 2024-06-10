d279dc0c990937a85f2eb4b2485953b33c2aafc3
nithigsn <nithishgsn000@gmail.com>
2024-06-10 22:46:11 +0530

    created home page

---
+++ src/Pages/WelcomePage.jsx
@@ -0,0 +1,43 @@
+
+
+export default function WelcomePage() {
+    return (
+
+        <div className="welcome">
+
+            <div className="welcome-container">
+
+
+                <div className="box">
+                    <div className="home-head">
+                        <div>
+                            Iocn
+                        </div>
+
+                        <div>Electrify</div></div>
+                    <div className="inner-box">
+                        <h2>Discover and Share Your Favourite Music</h2>
+                    </div>
+                    <div className="inner-box">
+                        <button>Login</button>
+                        <button>Sign Up</button></div>
+                </div>
+
+                <div className="welcome-img">
+                    <div className="img-box">
+
+
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div>
+
+            </div>
+
+        </div>
+
+    );
+}
