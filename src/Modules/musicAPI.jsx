a8f28ac86044be551b9284db4c2b41a7e5f1b067
nithigsn <nithishgsn000@gmail.com>
2024-06-25 11:08:01 +0530

    updated player

---
+++ src/Modules/musicAPI.jsx
@@ -0,0 +1,50 @@
+const musicAPI = [
+    {
+      songName: 'Chasing',
+      songArtist: 'NEFFEX',
+      songSrc: './Assets/songs/Chasing - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image1.jpg',
+      songRanks: 1
+    },
+    {
+      songName: 'AURORA - Runaway',
+      songArtist: 'Aurora Aksnes',
+      songSrc: './Assets/songs/AURORA - Runaway (Lyrics).mp3',
+      songAvatar: './Assets/Images/image4.jpg',
+      songRanks: 2
+    },
+    {
+      songName: 'Catch Me If I Fall',
+      songArtist: 'TEGNENT',
+      songSrc: './Assets/songs/Catch Me If I Fall - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image2.jpg',
+      songRanks: 3
+    },
+    {
+      songName: 'Inspired (Clean)',
+      songArtist: 'NEFFEX',
+      songSrc: './Assets/songs/Inspired (Clean) - NEFFEX.mp3',
+      songAvatar: './Assets/Images/image3.jpg',
+      songRanks: 4
+    },
+    {
+      songName: 'Baby doll',
+      songArtist: 'Kanika Kapoor',
+      songSrc: './Assets/songs/Baby doll [ slowed + reverb ] __ meet bros ,Kanika Kapoor __ jr santu.mp3',
+      songAvatar: './Assets/Images/image5.jpg'
+    },
+    {
+      songName: 'Soch',
+      songArtist: 'Hardy Sandhu',
+      songSrc: './Assets/songs/SOCH(Slowed+Reverbed) __ Hardy Sandhu.webm',
+      songAvatar: './Assets/Images/image6.jpg'
+    },
+    {
+      songName: 'Apna Bana Le',
+      songArtist: 'Arijit Singh',
+      songSrc: './Assets/songs/Apna Bana Le - Full Audio _ Bhediya _ Varun Dhawan, Kriti Sanon_ Sachin-Jigar,Arijit Singh,Amitabh B.webm',
+      songAvatar: './Assets/Images/image7.jpg'
+    }
+  ];
+  
+  module.exports=musicAPI;
